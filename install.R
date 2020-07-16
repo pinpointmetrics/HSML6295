b5e6b69a2cfab48b923b13e164bce6c8a8c5449e
pinpointmetrics <pinpointmetrics@gmail.com>
2020-07-16 11:40:53 -0400

    Add files via upload

--- install.R
+++ install.R
@@ -1,3 +1,3 @@
-install.packages("stargazer")
-install.packages("pROC")
-install.packages("caTools")
+install.packages("stargazer", repos='http://cran.us.r-project.org')
+install.packages("pROC", repos='http://cran.us.r-project.org')
+install.packages("caTools", repos='http://cran.us.r-project.org')
